8ab3596162e5b4eed735ee3eb825d54d9429c71a
Remco Stoeten <remcostoeten@hotmail.com>
2025-09-18 19:45:40 +0200

    chore: implemented standardization for codestyle

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    useTabs: false,
+    tabWidth: 4,
+    singleQuote: true,
+    semi: false,
+    trailingComma: 'none',
+    endOfLine: 'lf',
+    printWidth: 100,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    jsxSingleQuote: true,
+    bracketSameLine: false,
+    spaceBeforeFunctionParen: false,
+    jsxBracketSameLine: false,
+    proseWrap: 'always',
+    importOrder: ['^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true
+}
