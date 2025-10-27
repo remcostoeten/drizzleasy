f905392138dc14b873a28a12fdea460612aa114d
Remco Stoeten <remcostoeten@hotmail.com>
2025-10-27 21:44:28 +0100

    feat: add docs anal-ytics

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -10,7 +11,9 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>{children}
+<Analytics/>
+        </RootProvider>
       </body>
     </html>
   );
